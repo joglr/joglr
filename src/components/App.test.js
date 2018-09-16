10280f21ddd506161d3b09b53b5fd6587993e9e1
Jonas Røssum <jonaswehoo@gmail.com>
2018-09-16 21:59:08 +0200

    added app icon added dynamic icon generation cleaned up package.json added Dockerfile and now.json for proper deployment

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,6 +4,8 @@ import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import App from './App'
 
+jest.mock("react-ga")
+
 Enzyme.configure({ adapter: new Adapter() })
 
 it('renders without crashing', () => {
