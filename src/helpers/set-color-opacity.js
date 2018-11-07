1302857b474b9fc60688d3fd76d679280417568f
Jonas Røssum <jonaswehoo@gmail.com>
2018-11-07 21:27:06 +0100

    clean up

--- src/helpers/set-color-opacity.js
+++ src/helpers/set-color-opacity.js
@@ -1,7 +1,7 @@
 const setColorOpacity = (color, opacity) => {
   const values = color.match(/[a-zA-Z0-9]{2}/g).map(
     x => parseInt(x, 16)
-  )//?
+  )
   return `rgba(${values.join(', ')}, ${opacity})`
 }
 
