07cd7c4d29770283ce5b7e424c8385c1585a39ce
Jonas Røssum <jonaswehoo@gmail.com>
2019-01-22 22:11:35 +0100

    revert to asyncComponent, as Suspense does not work with ReactDOMServer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,33 @@
-import {
-  Button,
-  LinearProgress,
-  Tooltip,
-  Typography,
-  withStyles
-} from '@material-ui/core'
+import { Button, Tooltip, Typography, withStyles } from '@material-ui/core'
 import DownIcon from '@material-ui/icons/KeyboardArrowDownSharp'
 import LocationIcon from '@material-ui/icons/LocationOnSharp'
 import PropTypes from 'prop-types'
-import React, { lazy, Suspense } from 'react'
+import React from 'react'
 import pkg from './../../package.json'
 import joinClasses from './../helpers/join-classes'
 import setColorOpacity from './../helpers/set-color-opacity'
 
-const AsyncBackgroundParticles = lazy(() => import('./BackgroundParticles'))
-const AsyncAbout = lazy(() => import('./About'))
+import About from './About'
 
-import('typeface-roboto')
+import 'typeface-roboto'
+import asyncComponent from '../helpers/async-component'
+const BackgroundParticles = asyncComponent(() => import('./BackgroundParticles'))
 
 const absolute = {
-  position: 'absolute'
+  position: 'absolute',
 }
 
 const topBorderSize = 1
 
 const styles = theme => ({
   root: {
-    fontFamily: `'Roboto Mono', Consolas, monospace`
+    fontFamily: `'Roboto Mono', Consolas, monospace`,
   },
   section: {
     minHeight: '100vh',
     '&:first-child': {
       backgroundColor: 'transparent',
-      minHeight: `calc(100vh - ${topBorderSize * theme.spacing.unit}px)`
+      minHeight: `calc(100vh - ${topBorderSize * theme.spacing.unit}px)`,
     },
     '&:not(:first-child)': {
       boxSizing: 'border-box',
@@ -40,34 +35,34 @@ const styles = theme => ({
       borderTop: `${topBorderSize * theme.spacing.unit}px ${
         theme.palette.secondary.main
       } solid`,
-      backgroundColor: setColorOpacity(theme.palette.primary.main, 0.85)
-    }
+      backgroundColor: setColorOpacity(theme.palette.primary.main, 0.85),
+    },
   },
   paragraph: {
     margin: '0.5em 0 0.5em',
-    lineHeight: '150%'
+    lineHeight: '150%',
   },
   container: {
     maxWidth: '960px',
-    margin: 'auto'
+    margin: 'auto',
   },
   typographyWrapper: {
-    padding: '1rem'
+    padding: '1rem',
   },
   noUserSelect: {
-    userSelect: 'none'
+    userSelect: 'none',
   },
   display4: {
     color: 'inherit',
     '@media (max-width: 960px)': {
-      fontSize: 9 * theme.spacing.unit
-    }
+      fontSize: 9 * theme.spacing.unit,
+    },
   },
   display3: {
     color: 'inherit',
     '@media (max-width: 960px)': {
-      fontSize: 4 * theme.spacing.unit
-    }
+      fontSize: 4 * theme.spacing.unit,
+    },
   },
   w100: { fontWeight: 100 },
   w300: { fontWeight: 300 },
@@ -88,43 +83,43 @@ const styles = theme => ({
       bottom: '0',
       left: '0',
       borderBottom: `0.04em currentColor solid`,
-      transition: 'width .15s cubic-bezier(.5,0,0,1)'
+      transition: 'width .15s cubic-bezier(.5,0,0,1)',
     },
     '&:hover:after': {
-      width: '100%'
-    }
+      width: '100%',
+    },
   },
   textCentered: {
-    textAlign: 'center'
+    textAlign: 'center',
   },
   textIcon: {
     color: theme.palette.secondary.main,
     fontSize: '90%',
-    verticalAlign: 'baseline'
+    verticalAlign: 'baseline',
   },
   leftIcon: {
     // marginRight: '0.1em'
   },
   noWrap: {
-    whiteSpace: 'nowrap'
+    whiteSpace: 'nowrap',
   },
   centeredX: {
     ...absolute,
     left: '50%',
-    transform: 'translateX(-50%)'
+    transform: 'translateX(-50%)',
   },
   centeredY: {
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   centeredXY: {
     ...styles.centedX,
     ...styles.centedY,
-    transform: 'translate(-50%, -50%)'
+    transform: 'translate(-50%, -50%)',
   },
   keepWhiteSpace: {
-    whiteSpace: 'pre-wrap'
+    whiteSpace: 'pre-wrap',
   },
   themePrimary: { backgroundColor: theme.palette.primary.main },
   themePrimaryContrast: { backgroundColor: theme.palette.primary.contrastText },
@@ -132,10 +127,10 @@ const styles = theme => ({
   themePrimaryContrastText: { color: theme.palette.primary.contrastText },
   themeSecondary: { backgroundColor: theme.palette.secondary.main },
   themeSecondaryContrast: {
-    backgroundColor: theme.palette.secondary.contrastText
+    backgroundColor: theme.palette.secondary.contrastText,
   },
   themeSecondaryText: { color: theme.palette.secondary.main },
-  themeSecondaryContrastText: { color: theme.palette.secondary.contrastText }
+  themeSecondaryContrastText: { color: theme.palette.secondary.contrastText },
 })
 
 const App = ({ classes, ReactGA }) => {
@@ -147,9 +142,11 @@ const App = ({ classes, ReactGA }) => {
           classes.firstSection,
           classes.centeredY,
           classes.themePrimaryContrastText
-        )}>
+        )}
+      >
         <div
-          className={joinClasses(classes.typographyWrapper, classes.container)}>
+          className={joinClasses(classes.typographyWrapper, classes.container)}
+        >
           <Typography
             variant="display4"
             className={joinClasses(
@@ -157,7 +154,8 @@ const App = ({ classes, ReactGA }) => {
               classes.noUserSelect,
               classes.w100,
               classes.display4
-            )}>
+            )}
+          >
             Hi.
           </Typography>
           <Typography
@@ -167,7 +165,8 @@ const App = ({ classes, ReactGA }) => {
               classes.noUserSelect,
               classes.w300,
               classes.display3
-            )}>
+            )}
+          >
             <p>
               I'm{' '}
               <span className={classes.w400}>
@@ -195,23 +194,23 @@ const App = ({ classes, ReactGA }) => {
                 color="secondary"
                 variant="fab"
                 component="a"
-                href="#about">
+                href="#about"
+              >
                 <DownIcon />
               </Button>
             </Tooltip>
           </div>
         </div>
       </section>
-      <Suspense fallback={<LinearProgress />}>
-        <AsyncAbout classes={classes} ReactGA={ReactGA} />
-        <AsyncBackgroundParticles />
-      </Suspense>
+      <About classes={classes} ReactGA={ReactGA} />
+      {typeof window !== 'undefined' &&
+        <BackgroundParticles />}
     </div>
   )
 }
 
 App.propTypes = {
-  classes: PropTypes.object
+  classes: PropTypes.object,
 }
 
 export default withStyles(styles)(App)
