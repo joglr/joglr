d29c0342f2a6b7cf1462027ba8705c4089f551f3
joglr <jonas.g.roessum@gmail.com>
2020-05-23 20:14:00 +0200

    Add index page

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,18 +1,36 @@
-import { MuiThemeProvider } from "@material-ui/core/styles"
+import { ThemeProvider } from "@material-ui/core/styles"
 import React from "react"
-import App from "../components/App"
 import theme from "../theme"
+import Head from "next/head"
+import CssBaseline from "@material-ui/core/CssBaseline"
 import "./../components/layout.css"
 import "typeface-roboto"
 
-const IndexPage = () => (
-  <MuiThemeProvider {...{ theme }}>
-    <App />
-  </MuiThemeProvider>
-)
+export default function App(props) {
+  const { Component, pageProps } = props
 
-// <Layout>
-// {/* <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} /> */}
-// </Layout>
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side")
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
 
-export default IndexPage
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  )
+}
