cc974ced4ba0a83e83c964dfa4215f99cc82c527
joglr <jonas.g.roessum@gmail.com>
2020-12-23 02:48:01 +0100

    New website

--- src/helpers/analytics.js
+++
@@ -1,6 +0,0 @@
-import ReactGA from 'react-ga'
-
-if (process.env.NODE_ENV === 'production') {
-  ReactGA.initialize('UA-34976555-1')
-  ReactGA.pageview(window.location.pathname + window.location.search)
-}
