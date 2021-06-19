293076236c2016aac07c547548a9f70448515a9d
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2021-06-19 17:20:31 +0200

    Migrate to next SSR Add theme switch Support light / dark theme

--- src/util.test.ts
+++
@@ -1,9 +0,0 @@
-import { assertEquals } from "https://deno.land/std@0.82.0/testing/asserts.ts";
-import { linkify } from "./util.ts";
-
-Deno.test("linkify", () => {
-  const input = "Test [google](https://www.google.dk/)";
-  const expected = 'Test <a href="https://www.google.dk/">google</a>';
-  const actual = linkify(input);
-  assertEquals(expected, actual);
-});
